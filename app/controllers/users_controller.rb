d70967ba096114cfa2d0e0e415677b718a3d90cf
yamada7717 <hiro.program17@gmail.com>
2024-02-12 11:44:25 +0900

    ユーザーコントローラーの作成

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,25 @@
+class UsersController < ApplicationController
+  def show
+    @user = User.find(params[:id])
+  end
+
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      auto_login(@user)
+      redirect_to root_path, notice: 'ユーザー登録が完了しました'
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:email, :password, :password_confirmation, :name, :profile_image, :introduction)
+  end
+end
