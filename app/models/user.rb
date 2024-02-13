efd66a504ddfe4943c580db81b84559361aef6e8
yamada7717 <hiro.program17@gmail.com>
2024-02-13 19:39:18 +0900

    Userモデルにバリデーションを追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,9 @@
 class User < ApplicationRecord
   authenticates_with_sorcery!
+
+  validates :name, presence: true
+  validates :email, presence: true, uniqueness: true, format: { with: URI::MailTo::EMAIL_REGEXP }
+  validates :password, length: { minimum: 8 }, if: -> { new_record? || changes[:password] }
+  validates :password, confirmation: true, if: -> { new_record? || changes[:password] }
+  validates :password_confirmation, presence: true, if: -> { new_record? || changes[:password] }
 end
