82c9ac1309a22bb608f5d45d9373971b88654a38
yamada7717 <hiro.program17@gmail.com>
2024-02-13 20:07:46 +0900

    user_sessionsコントローラーの作成/ログインページの作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
   root 'tops#index'
+  get 'login', to: 'user_sessions#new'
+  post 'login', to: 'user_sessions#create'
+  get 'logout', to: 'user_sessions#destroy'
   resources :users, only: %i[new create show edit update destroy]
 end
