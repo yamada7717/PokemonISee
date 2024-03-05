2ba3c97ff42455821b3883874e195094a93dad21
yamada7717 <hiro.program17@gmail.com>
2024-03-05 20:01:55 +0900

    ユーザーモデルのテストを追加

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,58 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let(:user) { build(:user) }
+
+  describe '#valid' do
+    context '新規登録が成功する場合' do
+      it '属性が有効の場合' do
+        expect(user.valid?).to eq true
+      end
+    end
+
+    context '新規登録が失敗する場合' do
+      it '名前がない場合は無効' do
+        user.name = nil
+        user.valid?
+        expect(user.errors[:name]).to include("を入力してください")
+      end
+
+      it '名前が30文字より多い場合は無効' do
+        user.name = "a" * 31
+        user.valid?
+        expect(user.errors[:name]).to include("は30文字以内で入力してください")
+      end
+
+      it 'メールアドレスがない場合は無効' do
+        user.email = nil
+        user.valid?
+        expect(user.errors[:email]).to include("を入力してください")
+      end
+
+      it '不正な形式のメールアドレスの場合は無効' do
+        user.email = 'valid_email'
+        user.valid?
+        expect(user.errors[:email]).to include("の形式が不正です")
+      end
+
+      it 'パスワードが8文字より少ない場合' do
+        user.password = "miss"
+        user.valid?
+        expect(user.errors[:password]).to include("は8文字以上である必要があります")
+      end
+
+      it 'パスワードが一致しない時は無効' do
+        user.password = "password123"
+        user.password_confirmation = "misspassword"
+        user.valid?
+        expect(user.errors[:password_confirmation]).to include("がパスワードと一致しません")
+      end
+
+      it 'メールアドレスが重複している場合は無効' do
+        create(:user, email: user.email)
+        user.valid?
+        expect(user.errors[:email]).to include("は既に使用されています")
+      end
+    end
+  end
 end
--- spec/requests/user_sessions_spec.rb
+++ spec/requests/user_sessions_spec.rb
@@ -3,31 +3,31 @@ require 'rails_helper'
 RSpec.describe "UserSessions", type: :request do
   let!(:user) { create(:user) }
 
-  describe "GET /login" do
-    it "ログインページが正常に表示されること" do
+  describe 'GET /login' do
+    it 'ログインページが正常に表示されること' do
       get login_path
       expect(response).to have_http_status(:success)
     end
   end
 
-  describe "POST /login" do
-    context "正しい認証情報の場合" do
-      it "ログインに成功し、リダイレクトされること" do
+  describe 'POST /login' do
+    context '正しい認証情報の場合' do
+      it 'ログインに成功し、リダイレクトされること' do
         post login_path, params: { email: user.email, password: 'password123', password_confirmation: 'password123' }
         expect(response).to redirect_to(root_path)
       end
     end
 
-    context "誤った認証情報の場合" do
-      it "ログインに失敗し、ログインページに戻ること" do
+    context '誤った認証情報の場合' do
+      it 'ログインに失敗し、ログインページに戻ること' do
         post login_path, params: { email: user.email, password: 'wrongpassword', password_confirmation: 'wrongpassword' }
         expect(response).to have_http_status(:success)
       end
     end
   end
 
-  describe "DELETE /logout" do
-    it "ログアウトに成功し、ルートページにリダイレクトされること" do
+  describe 'DELETE /logout' do
+    it 'ログアウトに成功し、ルートページにリダイレクトされること' do
       get logout_path
       expect(response).to redirect_to(root_path)
       expect(session[:user_id]).to be_nil
