ff1bdd629ba3fcde24f372cc44628ee289887e04
yamada7717 <hiro.program17@gmail.com>
2024-03-10 08:36:48 +0900

    新規登録をする際の異常となるテストケースを追加

--- spec/system/users_spec.rb
+++ spec/system/users_spec.rb
@@ -1,19 +1,33 @@
 require 'rails_helper'
 
-RSpec.describe 'User management', type: :system do
+RSpec.describe 'User', type: :system do
   describe 'ユーザーの新規登録' do
     context 'フォームの入力値が正常なとき' do
       it 'ユーザー登録処理が成功する' do
-        visit login_path
+        visit new_user_path
         click_on "新規登録"
-        fill_in "name", with: "test_user"
-        fill_in "email", with: "test@example.com"
-        fill_in "password", with: "password123"
-        fill_in "password_confirmation", with: "password123"
+        fill_in "ユーザー名", with: "test_user"
+        fill_in "メールアドレス", with: "test@example.com"
+        fill_in "パスワード", with: "password123"
+        fill_in "パスワードの確認", with: "password123"
         click_button "会員登録"
 
         expect(page).to have_content "ユーザー登録が完了しました"
         expect(current_path).to eq(root_path)
+      end
+    end
+    context 'フォームの入力値入力値に異常がある場合' do
+      it 'ユーザー登録処理が失敗する' do
+        visit new_user_path
+        click_on "新規登録"
+        fill_in "ユーザー名", with: "test_user"
+        fill_in "メールアドレス", with: "miss_mail"
+        fill_in "パスワード", with: "password"
+        fill_in "パスワードの確認", with: "password123"
+        click_button "会員登録"
+
+        expect(page).to have_content "メールアドレス の形式が不正です"
+        expect(current_path).to eq(users_path)
       end
     end
   end
