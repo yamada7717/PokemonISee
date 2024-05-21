57015667abc84344a03a2f69c344cfcb15612e8a
yamada7717 <hiro.program17@gmail.com>
2024-05-21 22:05:46 +0900

    プロフィール更新ページ/更新機能の作成

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,13 +7,31 @@ class UsersController < ApplicationController
     @user = User.new
   end
 
+  def edit
+    @user = current_user
+  end
+
   def create
     @user = User.new(user_params)
     if @user.save
       auto_login(@user)
-      redirect_to root_path, notice: 'ユーザー登録が完了しました'
+      redirect_to root_path, notice: "ユーザー登録が完了しました"
     else
       render :new
+    end
+  end
+
+  def update
+    @user = current_user
+    if user_params[:password].blank?
+      user_params.delete(:password)
+      user_params.delete(:password_confirmation)
+    end
+
+    if @user.update(user_params)
+      redirect_to mypage_user_path(@user), notice: "ユーザー情報を更新しました"
+    else
+      render :edit
     end
   end
 
