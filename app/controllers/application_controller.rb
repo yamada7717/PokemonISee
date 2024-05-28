3eedd37f612f8c7972e86e18ba6571c4b595d514
yamada7717 <hiro.program17@gmail.com>
2024-05-29 02:04:10 +0900

    レコードが見つからない場合はTOPページに戻るよう設定

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,9 @@
 class ApplicationController < ActionController::Base
+  rescue_from ActiveRecord::RecordNotFound, with: :redirect_to_root
+
+  private
+
+  def redirect_to_root
+    redirect_to root_path, alert: "ページが見つからなためトップページに戻りました"
+  end
 end
