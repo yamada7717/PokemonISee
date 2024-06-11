c20c48c8c02e57e58dbd1d33602d497946fdde93
yamada7717 <hiro.program17@gmail.com>
2024-06-11 16:36:47 +0900

    構築記事登録の際のバリデーションの設定

--- app/models/build.rb
+++ app/models/build.rb
@@ -1,3 +1,9 @@
 class Build < ApplicationRecord
   belongs_to :user
+
+  validates :title, presence: true, length: { maximum: 20 }
+  validates :introduction, length: { maximum: 2000 }, allow_blank: true
+  validates :season, presence: true
+  validates :battle_type, presence: true
+  validates :is_public, inclusion: { in: [true, false] }
 end
