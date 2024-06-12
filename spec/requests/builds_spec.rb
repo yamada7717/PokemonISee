68772c315775f01e8d083a38950a1eea67c28327
yamada7717 <hiro.program17@gmail.com>
2024-06-12 14:28:00 +0900

    構築紹介投稿ページのリクエストスペックの追加

--- spec/requests/builds_spec.rb
+++ spec/requests/builds_spec.rb
@@ -1,7 +1,25 @@
 require 'rails_helper'
 
 RSpec.describe "Builds", type: :request do
-  describe "GET /index" do
-    pending "add some examples (or delete) #{__FILE__}"
+  let!(:user) { create(:user) }
+
+  describe "GET /builds/new" do
+    context "ログインしている場合" do
+      before do
+        post login_path, params: { email: user.email, password: 'password123' }
+      end
+
+      it "投稿ページに遷移できる" do
+        get new_build_path
+        expect(response).to have_http_status(:success)
+      end
+    end
+
+    context "ログインしていない場合" do
+      it "TOPページににリダイレクトされる" do
+        get new_build_path
+        expect(response).to redirect_to(root_path)
+      end
+    end
   end
 end
