662f249af5a96d0e3dcaa6ef01ce05aa44232d35
yamada7717 <hiro.program17@gmail.com>
2024-06-22 21:27:35 +0900

    構築記事詳細ページ・構築記事更新機能の追加

--- app/controllers/builds_controller.rb
+++ app/controllers/builds_controller.rb
@@ -1,16 +1,19 @@
 class BuildsController < ApplicationController
   before_action :require_login
+  before_action :set_build, only: %i[show edit update destroy]
 
   def index
     @builds = Build.all
   end
 
   def show
-    @build = Build.find(params[:id])
   end
 
   def new
     @build = Build.new
+  end
+
+  def edit
   end
 
   def create
@@ -19,12 +22,23 @@ class BuildsController < ApplicationController
       # 後ほどポケモン登録機能を実装するため、一旦builds_pathにリダイレクト
       redirect_to builds_path
     else
-      Rails.logger.debug @build.errors.full_messages
       render :new
     end
   end
 
+  def update
+    if @build.update(build_params)
+      redirect_to build_path(@build), notice: '構築記事が更新されました。'
+    else
+      render :edit
+    end
+  end
+
   private
+
+  def set_build
+    @build = Build.find(params[:id])
+  end
 
   def build_params
     params.require(:build).permit(:game_type, :title, :introduction, :season, :battle_type, :battle_rank, :battle_rate, :blog_url, :is_public)
