98daff232369dd94481d427a78a78835d4137bc9
yamada7717 <hiro.program17@gmail.com>
2024-07-02 23:19:11 +0900

    ポケモンパーティ登録に関する各テーブルの関連付け

--- app/models/build.rb
+++ app/models/build.rb
@@ -1,5 +1,6 @@
 class Build < ApplicationRecord
   belongs_to :user
+  has_many :pokemon_parties, dependent: :destroy
 
   validates :title, presence: true, length: { maximum: 20 }
   validates :introduction, length: { maximum: 2000 }, allow_blank: true
--- app/models/pokemon_party.rb
+++ app/models/pokemon_party.rb
@@ -1,2 +1,5 @@
 class PokemonParty < ApplicationRecord
+  belongs_to :build
+  belongs_to :pokemon
+  belongs_to :item
 end
