e1d5cd27beb3a4868bfce008a5e7218d20640b09
yamada7717 <hiro.program17@gmail.com>
2024-08-10 04:50:42 +0900

    いいねページのリクエストスペックの追加

--- spec/requests/likes_spec.rb
+++ spec/requests/likes_spec.rb
@@ -1,7 +1,23 @@
 require 'rails_helper'
 
 RSpec.describe "Likes", type: :request do
-  describe "GET /index" do
-    pending "add some examples (or delete) #{__FILE__}"
+  let(:user) { create(:user) }
+
+  before do
+    post login_path, params: { email: user.email, password: 'password123' }
+  end
+
+  describe "シングルバトル" do
+    it "シングルバトルいいねページに遷移できること" do
+      get single_battle_likes_user_path(user)
+      expect(response).to have_http_status(:success)
+    end
+  end
+
+  describe "ダブルバトル" do
+    it "ダブルバトルいいねページに遷移できること" do
+      get double_battle_likes_user_path(user)
+      expect(response).to have_http_status(:success)
+    end
   end
 end
