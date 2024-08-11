a7140ed29e66ca9a2b85d3ec762ad8a850f97a46
yamada7717 <hiro.program17@gmail.com>
2024-08-12 04:12:16 +0900

    いいねした記事のシステムスペックを追加

--- spec/system/likes_spec.rb
+++ spec/system/likes_spec.rb
@@ -15,14 +15,14 @@ RSpec.describe 'Likes', type: :system do
       click_button "ログイン"
     end
 
-    it 'ユーザーがシングルバトルの構築記事にいいねできること' do
+    it 'シングルバトルの構築記事にいいねできること' do
       visit build_path(single_battle_build)
       expect(page).to have_content('いいね')
       click_button 'いいね'
       expect(page).to have_button('いいねを外す')
     end
 
-    it 'ユーザーがシングルバトルの構築記事からいいねを外せること' do
+    it 'シングルバトルの構築記事からいいねを外せること' do
       liking_user.likes.create!(build: single_battle_build)
       visit build_path(single_battle_build)
       expect(page).to have_button('いいねを外す')
@@ -30,19 +30,43 @@ RSpec.describe 'Likes', type: :system do
       expect(page).to have_button('いいね')
     end
 
-    it 'ユーザーがダブルバトルの構築記事にいいねできること' do
+    it 'ダブルバトルの構築記事にいいねできること' do
       visit build_path(double_battle_build)
       expect(page).to have_content('いいね')
       click_button 'いいね'
       expect(page).to have_button('いいねを外す')
     end
 
-    it 'ユーザーがダブルバトルの構築記事からいいねを外せること' do
+    it 'ダブルバトルの構築記事からいいねを外せること' do
       liking_user.likes.create!(build: double_battle_build)
       visit build_path(double_battle_build)
       expect(page).to have_button('いいねを外す')
       click_button 'いいねを外す'
       expect(page).to have_button('いいね')
+    end
+
+    it 'シングルバトルいいねページに遷移できること' do
+      liking_user.likes.create!(build: single_battle_build)
+      visit single_battle_likes_user_path(liking_user)
+      expect(page).to have_current_path(single_battle_likes_user_path(liking_user))
+    end
+
+    it 'シングルバトルいいねページで1件登録がされていることを確認できること' do
+      liking_user.likes.create!(build: single_battle_build)
+      visit single_battle_likes_user_path(liking_user)
+      expect(page).to have_content(single_battle_build.title)
+    end
+
+    it 'ダブルバトルいいねページに遷移できること' do
+      liking_user.likes.create!(build: double_battle_build)
+      visit double_battle_likes_user_path(liking_user)
+      expect(page).to have_current_path(double_battle_likes_user_path(liking_user))
+    end
+
+    it 'ダブルバトルいいねページで1件登録がされていることを確認できること' do
+      liking_user.likes.create!(build: double_battle_build)
+      visit double_battle_likes_user_path(liking_user)
+      expect(page).to have_content(double_battle_build.title)
     end
   end
 
