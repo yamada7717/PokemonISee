004693434e0e9919616f3f2a7f1edeb44bff909e
yamada7717 <hiro.program17@gmail.com>
2024-08-30 18:56:41 +0900

    フラッシュメッセージの修正

--- app/controllers/password_resets_controller.rb
+++ app/controllers/password_resets_controller.rb
@@ -13,8 +13,8 @@ class PasswordResetsController < ApplicationController
   def create
     @user = User.find_by(email: params[:email])
     @user&.deliver_reset_password_instructions!
+    flash[:notice] = 'パスワードリセットのメールを送信しました'
     redirect_to login_path
-    flash.now[:notice] = 'パスワードリセットのメールを送信しました'
   end
 
   def update
@@ -32,11 +32,11 @@ class PasswordResetsController < ApplicationController
         redirect_to login_path
         flash[:notice] = 'パスワードがリセットされました。'
       else
-        flash.now[:alert] = 'パスワードリセットに失敗しました、再度お試しください。'
+        flash[:alert] = 'パスワードリセットに失敗しました、再度お試しください。'
         render :edit, status: :unprocessable_entity
       end
     else
-      flash.now[:alert] = '無効なリクエストです。'
+      flash[:alert] = '無効なリクエストです。'
       render :edit, status: :unprocessable_entity
     end
   end
