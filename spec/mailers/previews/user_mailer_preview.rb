73c569540bc42d1d3b4a7fe5ec1a066c51ca3cf3
yamada7717 <hiro.program17@gmail.com>
2024-09-01 16:47:08 +0900

    TOPページの紹介文の変更

--- spec/mailers/previews/user_mailer_preview.rb
+++ spec/mailers/previews/user_mailer_preview.rb
@@ -1,9 +1,7 @@
 # Preview all emails at http://localhost:3000/rails/mailers/user_mailer_mailer
 class UserMailerPreview < ActionMailer::Preview
-
   # Preview this email at http://localhost:3000/rails/mailers/user_mailer_mailer/reset_password_emai
   def reset_password_emai
     UserMailer.reset_password_emai
   end
-
 end
